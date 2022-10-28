6477432ed487225f6767ca6a41d6ab04b9b619da
Joan Defosse <joan.defosse@etu.univ-nantes.fr>
2022-10-28 17:47:52 +0200

    Début du projet

---
+++ Tree_pck/Tree.java
@@ -0,0 +1,11 @@
+package Tree_pck;
+
+import java.awt.Color;
+
+public class Tree {
+
+    private Tree L, R;
+    private boolean axis; // 0 si axe X, 1 si axe Y
+    private Integer x, y;
+    private Color col;
+}
