c1ec3cba18ad40b0b080f25f5f76d3516ce3d606
iceberg8 <maudeh975@gmail.com>
2022-11-15 16:22:26 +0100

    add class position

---
+++ TreePck/Position.java
@@ -0,0 +1,43 @@
+package TreePck;
+
+public class Position {
+    private int left;
+    private int right;
+    private int down;
+    private int up;
+
+    public Position(int left, int right, int down, int up) {
+        this.left = left;
+        this.right = right;
+        this.down = down;
+        this.up = up;
+    }
+
+    public int getLeft() {
+        return left;
+    }
+    public void setLeft(int left) {
+        this.left = left;
+    }
+
+    public int getRight() {
+        return right;
+    }
+    public void setRight(int right) {
+        this.right = right;
+    }
+
+    public int getDown() {
+        return down;
+    }
+    public void setDown(int down) {
+        this.down = down;
+    }
+
+    public int getUp() {
+        return up;
+    }
+    public void setUp(int up) {
+        this.up = up;
+    }
+}
